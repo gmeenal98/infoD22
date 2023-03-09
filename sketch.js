fb4a34d0b71bd0621cc1b17487bf3e6add146b6f
gmeenal98 <127397480+gmeenal98@users.noreply.github.com>
2023-03-09 10:52:10 +0000

    Added p5.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+    background(255);
+    
+    noFill();
+    strokeWeight(10);
+    
+    circle (0,150,450);
+    
+    circle (200,400,450);
+    
+    circle (400,0,500);
+  }
+  
+  
